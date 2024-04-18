404f4addf90fa71c312f81cc5a08dbb2c4725ddf
Christiaan C Cupido <christiaanccupido@gmail.com>
2024-04-18 14:16:55 +0200

    .

---
+++ main.js
@@ -0,0 +1,45 @@
+const msgerForm = get(".msger-inputarea");
+const msgerInput = get(".msger-input");
+const msgerChat = get(".msger-chat");
+const BOT_IMG = "bot.png";
+const PERSON_IMG = "bot.png";
+const BOT_NAME = "BOT";
+const PERSON_NAME = "CCupido"
+const prompts = [
+    ["Hi", "Hey", "Hello", "Good morning", "Good afternoon"],
+    ["How are you?", "How is life?", "How are things?"],
+    ["What are you doing?", "What is going on?", "What is up?"],
+    ["How old are you?"],
+    ["Who are you?", "Are you human?", "Are you a bot?", "Are you human or a bot?"],
+    ["Who created you?", "Who made you?"],
+    [ 
+        "Your name please",
+        "Your name?",
+        "May I know your name?",
+        "What is your name?",
+        "What do you call yourself"
+    ],
+    ["I love you"],
+    ["Happy", "Good", "Fun", "Wonderful", "Fantastic", "Cool"],
+    ["Bad", "Bored", "Tired"],
+    ["Help me", "Tell me a story", "Tell me a joke"],
+    ["Ah", "Yes", "Ok", "Okay", "Nice"],
+    ["Bye", "Good bye", "Goodbye", "See you later"],
+    ["What should I eat today?"],
+    ["Bro"],
+    ["What", "Why", "How", "Where", "When"],
+    ["No", "Not sure", "Maybe", "No thanks"],
+    [""],
+    ["Haha", "Ha", "Lol", "Hehe", "Funny", "Joke"],
+];
+const replies [
+
+];
+const alternative = [
+  "Same",
+  "Go on...",
+  "Bro...",
+  "Try again",
+  "I'm listening...",
+  "I don't understand :/"
+]
